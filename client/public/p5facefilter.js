ef8c4577a5ad0eb9f233c19fb5e26188d04ec8cb
AdamWarlord <anuragparcha315@gmail.com>
2021-08-11 11:28:47 +0530

    Heroku deploy fix

--- client/public/p5facefilter.js
+++ client/public/p5facefilter.js
@@ -1,5 +1,12 @@
 let outputWidth;
-let outputHeight;
+let outputHeight = 612;
+
+if ( navigator.platform != "iPad" && navigator.platform != "iPhone" && navigator.platform != "iPod" ) {
+  outputWidth = window.outerWidth; 
+      //I'll use window.innerWidth in production
+} else {
+  outputWidth = screen.width;
+}
 
 let faceTracker; // Face Tracking
 let videoInput; // Video object
@@ -45,7 +52,7 @@ document.getElementById('videoPlayback').onclick = () => {
     videoInput.elt.setAttribute('playsinline', true);
     videoInput.elt.setAttribute('webkit-playsinline', true);
     videoInput.size(outputWidth, outputHeight);
-    //videoInput.hide();
+    videoInput.hide();
 
     // Create button to take a picture
     button = createButton('Take Picture');
@@ -90,8 +97,8 @@ function setup()
 {
   const maxWidth = Math.min(windowWidth, windowHeight);
   pixelDensity(1);
-  outputWidth = maxWidth;
-  outputHeight = maxWidth * 0.75; // 4:3
+  //outputWidth = maxWidth;
+  //outputHeight = maxWidth * 0.75; // 4:3
 
   // select filter
   const sel = createSelect();
